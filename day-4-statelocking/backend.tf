3b4a0aaa64f84fd8f2257af8a94e857462cef0b5
Ajith N <ajithnl314@gmail.com>
2025-08-10 12:49:20 +0530

    update

--- day-4-statelocking/backend.tf
+++ day-4-statelocking/backend.tf
@@ -3,7 +3,9 @@ terraform {
     bucket = "buckettogithubs3"
      key    =  "day4-terraform.tfstate"
     region = "us-west-2"
-    use_lockfile = true #
+    #use_lockfile = true #s3 supports this features but terraform version is >1.10 latest version =>1.10
+dynamodb_table = "ajith" #any versions
+encrypt = true
   }
 }
 
--- day-4-statelocking/main.tf
+++ day-4-statelocking/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "name" {
     ami = "ami-04e08e36e17a21b56"
     instance_type = "t2.micro"
   tags = { 
-    name = "new"
+    name = "ehiuhjds"
   }
 
 }
