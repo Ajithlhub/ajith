0cec28cf533cffee3cc3702b1b96f845704922cb
Ajith N <ajithnl314@gmail.com>
2025-08-10 11:37:04 +0530

    backend

---
+++ day-4-statelocking/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "buckettogithubs3"
+     key    =  "day4-terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
--- day-4-statelocking/main.tf
+++ day-4-statelocking/main.tf
@@ -2,6 +2,16 @@ resource "aws_instance" "name" {
     ami = "ami-04e08e36e17a21b56"
     instance_type = "t2.micro"
   tags = { 
-    name= "new"
+    name = "new"
   }
+
+}
+
+
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name ="vpc"
+  }
+
 }
